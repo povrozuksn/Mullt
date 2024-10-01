c71e4fee8e957cdf4f7082d39f8a3c49def57142
povrozuksn <96878944+povrozuksn@users.noreply.github.com>
2024-10-01 19:31:27 +0400

    Человек передвигает ноги при ходьбе

--- main.cpp
+++ main.cpp
@@ -62,18 +62,18 @@ void drawCloud(int x, int y)
     txEllipse (x+250-80, 70, x+380-80, 140);
 }
 
-void drawMan(int x)
+void drawMan(int x, int xLegs, int xHands)
 {
 //x=660
     txSetColor(RGB(255, 210, 166));
     txSetFillColor(RGB(255, 210, 166));
-    txCircle(x+660-660, 430, 20);               //Голова
+    txCircle(x, 430, 20);               //Голова
     txSetColor(RGB(255, 210, 166), 7);
-    txLine(x+660-660, 450, x+660-660, 520);           //Тело
-    txLine(x+660-660, 465, x+625-660, 490);           //Левая рука
-    txLine(x+660-660, 465, x+690-660, 490);           //Правая рука
-    txLine(x+660-660, 520, x+640-660, 560);           //Левая нога
-    txLine(x+660-660, 520, x+680-660, 560);           //Правая нога
+    txLine(x, 450, x, 520);           //Тело
+    txLine(x, 465, x-35+xHands, 490);           //Левая рука
+    txLine(x, 465, x+30-xHands, 490);           //Правая рука
+    txLine(x, 520, x-20+xLegs, 560);           //Левая нога
+    txLine(x, 520, x+20-xLegs, 560);           //Правая нога
 }
 
 void drawTree(int x, int y)
@@ -89,6 +89,20 @@ void drawTree(int x, int y)
     txEllipse (x+80-110, y+190-485, x+150-110, y+420-485);
 }
 
+void drawGrib(double razm)
+{
+
+    txSetColor(TX_BLACK);
+    txSetFillColor(RGB(185, 122, 87));
+    txEllipse (610, 460, 640*razm, 510*razm);
+
+    txSetColor(TX_BLACK);
+    txSetFillColor(TX_BLACK);
+    txEllipse(600, 430, 660*razm, 470*razm);
+
+}
+
+
 int main()
 {
 txCreateWindow (800, 600);
@@ -97,6 +111,9 @@ txCreateWindow (800, 600);
     int ySun = -100;
     int xCloud = 80;
     int xMan = 660;
+    int xManLegs = 0;
+    int xManHands = 0;
+    double razGrib = 1;
 
     while (xSun<1000)
     {
@@ -109,13 +126,36 @@ txCreateWindow (800, 600);
         drawTree(110, 485);
         drawTree(190, 450);
         drawTree(30, 450);
-        drawMan(xMan);
+        drawGrib(razGrib);
+        drawMan(xMan, xManLegs, xManHands);
         txEnd();
-
         xSun = xSun + 5;
         ySun = ySun + 3;
         xMan = xMan - 3;
-        txSleep(10);
+        xManLegs = xManLegs + 10;
+        xManHands = xManHands + 10;
+        razGrib = razGrib + 0.0005;
+        txSleep(50);
+
+        txBegin();
+        drawSky(RGB(162, 247, 251));
+        drawSun(xSun, ySun);
+        drawCloud(xCloud, 70);
+        drawLand(RGB(140, 255, 140));
+        drawHouse(RGB(217, 240, 247));
+        drawTree(110, 485);
+        drawTree(190, 450);
+        drawTree(30, 450);
+        drawGrib(razGrib);
+        drawMan(xMan, xManLegs, xManHands);
+        txEnd();
+        xSun = xSun + 5;
+        ySun = ySun + 3;
+        xMan = xMan - 3;
+        xManLegs = xManLegs - 10;
+        xManHands = xManHands - 10;
+        razGrib = razGrib + 0.0005;
+        txSleep(50);
     }
 
     while (xCloud<1000)
@@ -129,7 +169,7 @@ txCreateWindow (800, 600);
         drawTree(110, 485);
         drawTree(190, 450);
         drawTree(30, 450);
-        drawMan(xMan);
+        drawMan(xMan, xManLegs, xManHands);
         txEnd();
 
         xCloud = xCloud + 1;
